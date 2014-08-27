ca0f3c20862a353e63c88ce6813a810a9e20bda1
fisto <cat.fisto@gmail.com>
2014-08-27 12:15:19 +0900

    Sinatraメソッドだけ作った

--- sinatra/app.rb
+++ sinatra/app.rb
@@ -8,7 +8,44 @@ require 'cgi'
 require 'digest/md5'
 # load 'database.rb'
 
+#set :port, 80
+
+#Top Page
 get '/' do
     "Hello World!"
 end
 
+#Sign up Page
+get '/regist' do
+
+end
+
+#My Page
+get '/mypage' do
+
+end
+
+#IloveYou Page
+get 'love' do
+
+end
+
+#Conversation Page
+get 'conversation' do
+
+end
+
+#Regist
+post '/useradd' do
+
+end
+
+#Question
+post '/question' do
+
+end
+
+#Answer
+post '/answer' do
+
+end
