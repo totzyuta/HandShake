03fac3e134c5f409fc258f4a2019373801a481fb
fisto <cat.fisto@gmail.com>
2014-08-28 01:24:15 +0900

    自作Model作ってFormタグで確かめたりテストデータ入れたりした

--- sinatra/db.rb
+++ sinatra/db.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'thin'
 require 'mysql'
+require 'date'
 
 $db = Mysql::connect('localhost', ENV['DB_HANDSHAKE'] , ENV['DB_HANDSHAKE_PASS'], 'handshake')
 
@@ -15,3 +16,108 @@ end
 #connect
 $db.query 'use handshake'
 DBconsole('show tables')
+
+#model
+def useradd(name, sex, mail, photo)
+  sql = 'insert into users values(
+    NULL ,"' +
+    name + '",' +
+    sex.to_s + ',"' +
+    mail + '","' +
+    photo + '",
+    "",
+    "")'
+  $db.query sql
+end
+
+def userget(id)
+  sql = 'select * from users where user_id = "' + id.to_s + '"'
+  rs = $db.query sql
+  rs.each{|col|
+    return col#.join("\t")
+  }
+end
+
+def friendadd(my_id, friend_id)
+  sql = 'insert into friends values(' +
+    my_id.to_s + ',' +
+    friend_id.to_s + ')'
+  $db.query sql
+end
+
+def friendget(my_id)
+  sql = 'select * from friends where user_id = "' + my_id.to_s + '"'
+  rs = $db.query sql
+  result = []
+  rs.each{|col|
+    result << col#.join("\t")
+  }
+  return result
+end
+
+#my_id が target_idに告白する
+def iloveyou(my_id, target_id, handle, point, manifest)
+  sql = 'insert into approaches values(
+    NULL ,' +
+    my_id.to_s + ',' +
+    target_id.to_s + ',"' +
+    handle + '","' +
+    point + '","' +
+    manifest + '",' +
+    'now())'
+  $db.query sql
+end
+
+#my_idが告白してる人
+def gettarget(my_id)
+  sql = 'select * from approaches where user_id = "' + my_id.to_s + '"'
+  rs = $db.query sql
+  rs.each{|col|
+    return col#.join("\t")
+  }
+end
+
+#my_id に告白してる人
+def getlover(my_id)
+  sql = 'select * from approaches where target_id = "' + my_id.to_s + '"'
+  rs = $db.query sql
+  rs.each{|col|
+    return col#.join("\t")
+  }
+end
+
+#質問集の追加
+def questionadd(body)
+  sql = 'insert into questions values(
+    NULL ,"' +
+    body + '")'
+  $db.query sql
+end
+
+#質問を送る
+def conversationadd(approach_id, question_id)
+  sql = 'insert into conversations values(
+    NULL ,' +
+    approach_id.to_s + ',' +
+    question_id.to_s + ',' +
+    'now()' + ',"","")'
+  $db.query sql
+end
+
+#質問に答える
+def conversationup(conversation_id, answer)
+  sql = 'update conversations set
+    answer = "' + answer + '",
+    answer_at = now()
+    where conversation_id = "' + conversation_id.to_s + '"'
+  $db.query sql
+end
+
+#質問を照会
+def conversationget(conversation_id)
+  sql = 'select * from conversations where conversation_id = "' + conversation_id.to_s + '"'
+  rs = $db.query sql
+  rs.each{|col|
+    return col#.join("\t")
+  }
+end
--- sinatra/migrate.rb
+++ sinatra/migrate.rb
@@ -17,18 +17,24 @@ rs.each{|colmuns|
 db.query 'create table users(
   user_id int not null auto_increment primary key,
   name varchar(50),
-  family_name varchar(10),
-  first_name varchar(10),
   sex tinyint,
   mail varchar(100),
   photo varchar(255),
   created_at datetime,
   modified_at datetime)'
 
+db.query 'create table friends(
+    user_id int,
+    friend_id int
+  )'
+
 db.query 'create table approaches (
   approach_id int not null auto_increment primary key,
   user_id int,
   target_id int,
+  handle varchar(30),
+  point varchar(50),
+  manifest varchar(50),
   approached_at datetime)'
 
 db.query 'create table questions (
@@ -43,11 +49,56 @@ db.query 'create table conversations (
   answer varchar(20),
   answer_at datetime)'
 
+#test data
+useradd('TANAKA',1,'example@mail.com','');
+useradd('SATOU',1,'example@mail.com','');
+useradd('SUZUKI',0,'example@mail.com','');
+useradd('MIYANAGA',0,'example@mail.com','');
+
+friendadd(1,2)
+friendadd(1,3)
+friendadd(1,4)
+friendadd(2,3)
+friendadd(2,4)
+friendadd(3,4)
+friendadd(4,2)
+friendadd(4,3)
+
+p userget(3)
+
+friendget(1).each do |i|
+  p i
+end
+
+iloveyou(1,3,"","","")
+p gettarget(1)
+p getlover(3)
+
+#Questions
+questionadd("好きな食べ物は？")
+questionadd("好きな場所は？")
+questionadd("明日世界が滅びるなら何をする？")
+questionadd("よく読む本はどんなジャンル？")
+questionadd("先週の日曜日は何をしてた？")
+questionadd("よく使うスマホアプリは？")
+questionadd("スポーツしてる？")
+questionadd("好きな素数は？")
+questionadd("ペットは飼ってる？")
+
+conversationadd(1,1)
+p conversationget(1)
+conversationup(1,"ショートケーキの上のいちご")
+p conversationget(1)
+
 #user test Data
 =begin
 puts "= user table test data insert ="
 1.times{|i|
-  sql = 'insert into user values(NULL ,'+(261606986+i).to_s+' ,"test'+i.to_s+'", "user'+i.to_s+'","")'
+  sql = 'insert into user values(
+    NULL ,'+
+    (261606986+i).to_s+' ,"
+    test'+i.to_s+'", "
+    user'+i.to_s+'","")'
   db.query sql
 }
 
