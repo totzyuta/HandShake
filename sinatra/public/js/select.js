7734ef27b6d4881de3878a8d65857c7ee6ce36a3
aidy <aidy.91614@gmail.com>
2014-08-29 08:54:00 +0900

    select.jsでblurを削除

--- sinatra/public/js/select.js
+++ sinatra/public/js/select.js
@@ -5,11 +5,6 @@ $(function(){
     $(".box").css("top", "10px");
     $(".select").css("top", "0px");
 
-  }).blur(function(){
-    $(".line-out").css("display", "block");
-    $(".box").css("top", "30px");
-    $(".select").css("top", "30px");
-
   }).keyup(function(){
     var word = tar.val();
     if(word != ""){
