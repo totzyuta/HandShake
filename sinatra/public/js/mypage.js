46175eb138c5c5334ffa46ff7762d6a1abfcc2ea
aidy <aidy.91614@gmail.com>
2014-08-29 05:07:25 +0900

    mypageのボタンに波紋を作成

--- sinatra/public/js/mypage.js
+++ sinatra/public/js/mypage.js
@@ -1,5 +1,49 @@
+var sizeReset = function(){
+$(".anim-circle").css({
+      width: "60px",
+      height: "60px",
+      top: "+=35px",
+      left: "+=35px",
+      opacity: 1,
+      "border-radius": "130px",
+      border: "10px solid white",
+      });
+};
+
 $(function(){
   setInterval(function(){
-    console.log("aaaaaa");
-  }, 1000);
+    $(".anim-circle").animate({
+      width: "130px",
+      height: "130px",
+      top: "-=35px",
+      left: "-=35px",
+      opacity: 0.0,
+      "border-width": "1px",
+    }, 2000, "linear", sizeReset);
+  }, 3000);
+});
+
+var sizeReset2 = function(){
+$(".anim-circle2").css({
+      width: "60px",
+      height: "60px",
+      top: "+=35px",
+      left: "+=35px",
+      opacity: 1,
+      "border-radius": "130px",
+      border: "10px solid #ec902e",
+      });
+};
+
+$(function(){
+  setInterval(function(){
+    $(".anim-circle2").animate({
+      width: "130px",
+      height: "130px",
+      top: "-=35px",
+      left: "-=35px",
+      opacity: 0.0,
+      "border-width": "1px",
+    }, 2000, "linear", sizeReset2);
+  }, 3000);
 });
