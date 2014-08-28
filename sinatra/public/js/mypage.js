f91ca597a05f061fe61bf88547569092d0f98364
aidy <aidy.91614@gmail.com>
2014-08-29 06:04:23 +0900

    バーの点滅

--- sinatra/public/js/mypage.js
+++ sinatra/public/js/mypage.js
@@ -2,48 +2,68 @@ var sizeReset = function(){
 $(".anim-circle").css({
       width: "60px",
       height: "60px",
-      top: "+=35px",
-      left: "+=35px",
+      top: "+=30px",
+      left: "+=30px",
       opacity: 1,
       "border-radius": "130px",
       border: "10px solid white",
       });
 };
 
+var sizeResetLine = function(){
+$(".anim-line").css({
+      height: "52px",
+      top: "+=4px",
+      opacity: 1,
+      });
+};
+
 $(function(){
   setInterval(function(){
     $(".anim-circle").animate({
-      width: "130px",
-      height: "130px",
-      top: "-=35px",
-      left: "-=35px",
+      width: "120px",
+      height: "120px",
+      top: "-=30px",
+      left: "-=30px",
       opacity: 0.0,
       "border-width": "1px",
-    }, 2000, "linear", sizeReset);
+    }, 3000, "linear", sizeReset);
   }, 3000);
+  
+  setInterval(function(){
+    $(".anim-circle2").animate({
+      width: "120px",
+      height: "120px",
+      top: "-=30px",
+      left: "-=30px",
+      opacity: 0.0,
+      "border-width": "1px",
+    }, 3000, "linear", sizeReset2);
+  }, 3000);
+  
+  setInterval(function(){
+    var elem = $(".anim-line");
+    $(elem).animate({
+      height: "52px",
+      top: "-=0px",
+      opacity: 0.2,
+    }, 600, "linear", function(){
+      $(elem).animate({
+        opacity: 1
+      }, 600);
+    });
+  }, 1200);
+  
 });
 
 var sizeReset2 = function(){
 $(".anim-circle2").css({
       width: "60px",
       height: "60px",
-      top: "+=35px",
-      left: "+=35px",
+      top: "+=30px",
+      left: "+=30px",
       opacity: 1,
       "border-radius": "130px",
       border: "10px solid #ec902e",
       });
 };
-
-$(function(){
-  setInterval(function(){
-    $(".anim-circle2").animate({
-      width: "130px",
-      height: "130px",
-      top: "-=35px",
-      left: "-=35px",
-      opacity: 0.0,
-      "border-width": "1px",
-    }, 2000, "linear", sizeReset2);
-  }, 3000);
-});
