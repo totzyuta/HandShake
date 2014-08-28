4ec113a05cd80853e489faf178e791a51c2a5d93
aidy <aidy.91614@gmail.com>
2014-08-29 06:46:20 +0900

    いろいろ

---
+++ sinatra/public/js/conversation_from.js
@@ -0,0 +1,8 @@
+$(function(){
+  $(".conversation").each(function(){
+    $this = $(this);
+    var questionHeight = $this.find(".question").css("height");
+    var answerHeight = $this.find(".answer").css("height");
+    $this.find(".loverimg")
+  });
+});
